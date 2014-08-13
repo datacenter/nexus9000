6ccb041303caeb27e4d68922d8b0bf96e0a141b3
mscohen02 <mscohen02@gmail.com>
2014-08-13 11:19:02 -0700

    Moving ACI to separate Repo

--- aci/python/basics/createTenant.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append('pysdk')
-from insieme.mit import access
-access.rest()
-directory = access.MoDirectory(ip='172.21.128.100', port='8000', user='admin', password='password')
-polUni = directory.lookupByDn('uni')
-fvTenantMo = directory.create('fv.Tenant', polUni, name='Tenant1')
-d = directory.commit(polUni)
