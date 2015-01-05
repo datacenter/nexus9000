e1411c4fe0d51eb61a57613d9b1a32ec857e9857
Gopalasivam <gopal@onecloudinc.com>
2015-01-05 14:08:20 +0530

    Added Initial directory for nexus scripts with sample file

---
+++ nexusscripts/sample_script.py
@@ -0,0 +1,22 @@
+"""Script Cataloging Information
+:Product Info:Nexus::9000::9516::NX-OS Release 6.2
+:Category:Monitoring
+:Title:Interface Monitoring
+:Short Description:This script is to monitor Interface counters.
+:Long Description:This script is to monitor Interface counters like
+Drops, Errors, Utilization.
+:Input:Interface ID
+:Output:Drop, Errors and Utilization Status.
+"""
+
+import os
+
+def main():
+    """
+    This method will do the monitoring functionality 
+    """
+    print "Printing Status"
+
+
+if __name__ == '__main__':
+    main()
