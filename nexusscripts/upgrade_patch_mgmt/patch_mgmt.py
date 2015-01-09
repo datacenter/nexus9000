4395d23526500c8fef643d098e1b061078aefb42
ubuntu <ubuntu@gopi-ubuntu.(none)>
2015-01-09 14:09:18 -0800

    Software upgrade and patch-managementi changes.

---
+++ nexusscripts/upgrade_patch_mgmt/patch_mgmt.py
@@ -0,0 +1,201 @@
+"""Script Cataloging Information
+:Product Info:Nexus::9000::9516::NX-OS Release 6.2
+:Category:OS Software Patch Management
+:Title:Patch Management
+:Short Description:This script is to perform patch related operations
+:Long Description:This script is to perform operations like 
+    copy, activate, deactivate, remove patch files.
+:Input: N9K Address, username, password, tftp_address, smu_file, action
+:Output:status/result of the patch management action
+"""
+
+import argparse
+import getpass
+import sys
+
+sys.path.append("../../nx-os/nxapi/utils")
+
+from nxapi_utils import NXAPI
+import xmltodict
+
+
+class Args(object):
+    def __init__(self, args): 
+        self.n9k = args.hostname
+        self.username = args.username
+        self.password = args.password
+        if not self.password:
+            self.password = getpass.getpass()
+        self.tftp_address = args.tftp_address
+        self.smu_filename = args.smu_filename
+        self.action = args.action
+
+
+def initialize_nxapi_handler(params): 
+    nxapi = NXAPI()
+    nxapi.set_target_url('http://' + params.n9k  + '/ins')
+    nxapi.set_username(params.username)
+    nxapi.set_password(params.password)
+    nxapi.set_msg_type('cli_conf')
+    return nxapi
+
+
+def check_status(dict_res):
+    if dict_res['ins_api']['outputs']['output']['code'] == '200' and \
+        dict_res['ins_api']['outputs']['output']['msg'] == 'Success':
+        print dict_res['ins_api']['outputs']['output']['body']
+        return True
+    else:
+        print 'Error Msg:' + dict_res['ins_api']['outputs']['output']['msg']
+        print 'Code:' + dict_res['ins_api']['outputs']['output']['code']
+        return False
+
+
+def copy_patch_file(params, nxapi_handler):
+    print 'copy tftp://'+ params.tftp_address + '/' + params.smu_filename +\
+        ' bootflash:// vrf management'
+    nxapi_handler.set_cmd('copy tftp://'+ params.tftp_address + '/' +
+        params.smu_filename + ' bootflash:// vrf management')
+    returnData = nxapi_handler.send_req()
+    dict_res = xmltodict.parse(returnData[1])
+    return check_status(dict_res)
+    
+
+def list_patch_file(params, nxapi_handler):
+    print 'dir  bootflash:' + params.smu_filename
+    nxapi_handler.set_cmd('dir  bootflash:' + params.smu_filename)
+    returnData = nxapi_handler.send_req()
+    dict_res = xmltodict.parse(returnData[1])
+    return check_status(dict_res)
+
+
+def add_module(params, nxapi_handler):
+    print 'install add bootflash:' + params.smu_filename
+    nxapi_handler.set_cmd('install add bootflash:' + params.smu_filename)
+    returnData = nxapi_handler.send_req()
+    dict_res = xmltodict.parse(returnData[1])
+    return check_status(dict_res)
+
+
+def list_inactive_modules(nxapi_handler):
+    print 'show install inactive'
+    nxapi_handler.set_cmd('show install inactive')
+    returnData = nxapi_handler.send_req()
+    dict_res = xmltodict.parse(returnData[1])
+    return check_status(dict_res)
+
+
+def activate_patch_file(params, nxapi_handler):
+    print 'install activate ' + params.smu_filename
+    nxapi_handler.set_cmd('install activate ' + params.smu_filename)
+    returnData = nxapi_handler.send_req()
+    dict_res = xmltodict.parse(returnData[1])
+    return check_status(dict_res)
+
+ 
+def list_active_modules(nxapi_handler):
+    nxapi_handler.set_cmd('show install active')
+    returnData = nxapi_handler.send_req()
+    dict_res = xmltodict.parse(returnData[1])
+    return check_status(dict_res)
+
+
+def deactivate_patch_file(params, nxapi_handler):
+    print 'install deactivate ' + params.smu_filename
+    nxapi_handler.set_cmd('install deactivate ' + params.smu_filename)
+    returnData = nxapi_handler.send_req()
+    dict_res = xmltodict.parse(returnData[1])
+    return check_status(dict_res)
+
+ 
+def remove_modules(params, nxapi_handler):
+    nxapi_handler.set_cmd('install remove ' + params.smu_filename + ' forced')
+    returnData = nxapi_handler.send_req()
+    dict_res = xmltodict.parse(returnData[1])
+    return check_status(dict_res)
+
+
+def show_install_log(nxapi_handler):
+    nxapi_handler.set_cmd('show install log detail')
+    returnData = nxapi_handler.send_req()
+    dict_res = xmltodict.parse(returnData[1])
+    return check_status(dict_res)
+
+
+def initialize_args():
+
+        parser = argparse.ArgumentParser(
+              description='Nexus 9000 OS software patch management utility.',
+              epilog="""to openstack cluster.""")
+
+        parser.add_argument('--n9k', '-i', dest='hostname',
+            help='Nexus 9XXX hostname or ip address', required=True)
+        parser.add_argument('--user', '-u', dest='username',
+            help='Username to login to Nexus 9XXX switch', required=True)
+        parser.add_argument('--password', '-p', dest='password',
+            help='Password to login to UCS Manager')
+        parser.add_argument('--tftp_address', '-t', dest='tftp_address',
+            help='Tftp server ip-address.')
+        parser.add_argument('--smu_filename', '-f', dest='smu_filename',
+            help='SMU filename.', required=True)
+        parser.add_argument('--action', '-o', dest='action', 
+            help='Action Install/Remove patch file.', required=True, 
+            choices = ['copy', 'activate', 'deactivate', 'remove'])
+        args = parser.parse_args()
+        return Args(args)
+
+
+if __name__ == '__main__':
+
+    params = initialize_args()
+
+    nxapi_handler = initialize_nxapi_handler(params)
+
+    if params.action == 'copy':
+        if not params.tftp_address:
+            params.tftp_address = raw_input('tftp server address:')
+        if not copy_patch_file(params, nxapi_handler):
+            print 'Failed to copy patch file'
+            if not list_patch_file(params, nxapi_handler):
+                print 'Failed to copy patch file'
+            else:
+                print 'Patch file:' + params.smu_filename + ' already present' 
+            exit(-1)
+        if not list_patch_file(params, nxapi_handler):
+            print 'Failed to copy patch file'
+            exit(-1)
+    elif params.action == 'activate':
+        if not add_module(params, nxapi_handler):
+            print 'Failed to activate module.'
+            exit(-1)
+        if not list_inactive_modules(nxapi_handler):
+            print 'Failed to list inactive modules.'
+            exit(-1)
+        if not activate_patch_file(params, nxapi_handler):
+            print 'Failed to activate patch file.'
+            exit(-1)
+        if not list_active_modules(nxapi_handler):
+            print 'Failed to list active modules'
+            exit(-1)
+    elif params.action == 'deactivate':
+        if not list_active_modules(nxapi_handler):
+            print 'Failed to list active modules'
+            exit(-1)
+        if not deactivate_patch_file(params, nxapi_handler):
+            print 'Failed to deactivate patch file.'
+            exit(-1)
+        if not list_inactive_modules(nxapi_handler):
+            print 'Failed to list inactive modules.'
+            exit(-1) 
+    elif params.action == 'remove':
+        if not list_inactive_modules(nxapi_handler):
+            print 'Failed to list inactive modules.'
+            exit(-1) 
+        if not remove_modules(params, nxapi_handler):
+            print 'Failed to remove module.'
+            exit(-1)
+
+    if not show_install_log(nxap_handleri):
+        print 'Failed to get install log.'
+        exit(-1)
+    exit(0)
---
+++ nexusscripts/upgrade_patch_mgmt/sw_mgmt.py
@@ -0,0 +1,163 @@
+"""Script Cataloging Information
+:Product Info:Nexus::9000::9516::NX-OS Release 6.2
+:Category:OS Software Upgrade Management
+:Title: Software Management
+:Short Description:This script is to perform Software management operations 
+:Long Description:This script is to perform operations like 
+    copy, upgrade, downgrade, remove switch images.
+:Input: N9K Address, username, password, tftp_address, image_filename, action
+:Output:status/result of the software management action
+"""
+
+import argparse
+import getpass
+import sys
+
+sys.path.append("../../nx-os/nxapi/utils")
+from nxapi_utils import *
+from xmltodict import *
+
+
+class Args(object):
+
+    def __init__(self, args): 
+        self.n9k = args.hostname
+        self.username = args.username
+        self.password = args.password
+        if not self.password:
+            self.password = getpass.getpass()
+        self.tftp_address = args.tftp_address
+        self.image_filename = args.image_filename
+        self.action = args.action
+
+
+def initialize_nxapi_handler(params):
+
+    thisNXAPI = NXAPI()
+    thisNXAPI.set_target_url('http://' + params.n9k +'/ins')
+    thisNXAPI.set_username(params.username)
+    thisNXAPI.set_password(params.password)
+    thisNXAPI.set_timeout(1200)
+    thisNXAPI.set_msg_type('cli_conf')
+    return thisNXAPI
+
+
+def check_status(dict_res):
+
+    if dict_res['ins_api']['outputs']['output']['code'] == '200' and \
+        dict_res['ins_api']['outputs']['output']['msg'] == 'Success':
+        print dict_res['ins_api']['outputs']['output']['body']
+        return True
+    else:
+        print 'Error Msg:' + dict_res['ins_api']['outputs']['output']['msg']
+        print 'Code:' + dict_res['ins_api']['outputs']['output']['code']
+        return False
+
+
+def copy_image_file(params, nxapi_handler):
+
+    print 'copy tftp://'+ params.tftp_address + '/' + params.image_filename +\
+        ' bootflash:// vrf management'
+    nxapi_handler.set_cmd('copy tftp://'+ params.tftp_address + '/' +\
+        params.image_filename + ' bootflash:// vrf management')
+    return_xml = nxapi_handler.send_req()
+    dict_res = xmltodict.parse(return_xml[1])
+    return check_status(dict_res)
+
+
+def verify_active_sessions(nxapi_handler):
+    print 'show configuration session summary'
+    nxapi_handler.set_cmd('show configuration session summary')
+    return_xml = nxapi_handler.send_req()
+    dict_res = xmltodict.parse(return_xml[1])
+    return check_status(dict_res)
+
+
+def check_image_incompatability(params, nxapi_handler):
+    nxapi_handler.set_cmd('show incompatability nxos bootflash:' +\
+        params.image_filename)
+    return_xml = nxapi_handler.send_req()
+    dict_res = xmltodict.parse(return_xml[1])
+    return check_status(dict_res)
+
+
+def check_install_all_impact(params, nxapi_handler):
+    print 'show install all impact nxos bootflash:' +\
+        params.image_filename
+    nxapi_handler.set_cmd('show install all impact nxos bootflash:' +\
+        params.image_filename)
+    return_xml = nxapi_handler.send_req()
+    dict_res = xmltodict.parse(return_xml[1])
+    return check_status(dict_res)
+    
+
+def copy_run_cfg_start_cfg(nxapi_handler):
+    print 'copy running-config startup-config'
+    nxapi_handler.set_cmd('copy running-config startup-config')
+    return_xml = nxapi_handler.send_req()
+    dict_res = xmltodict.parse(return_xml[1])
+    return check_status(dict_res)
+
+
+def install_all_nxos_image(params, nxapi_handler):
+    print 'install all parallel nxos bootflash:' +\
+        params.image_filename
+    nxapi_handler.set_cmd('install all parallel nxos bootflash:' +\
+        params.image_filename)
+    return_xml = nxapi_handler.send_req()
+    dict_res = xmltodict.parse(return_xml[1])
+    return check_status(dict_res)
+
+
+def initialize_args():
+
+    parser = argparse.ArgumentParser(
+                description='Nexus 9000 OS software patch management utility.',
+                epilog="""   """)
+
+    parser.add_argument('--n9k', '-i', dest='hostname',
+        help='Nexus 9XXX hostname or ip address', required=True)
+    parser.add_argument('--user', '-u', dest='username',
+        help='Username to login to Nexus 9XXX switch', required=True)
+    parser.add_argument('--password', '-p', dest='password',
+        help='Password to login to UCS Manager')
+    parser.add_argument('--tftp-address', '-t', dest='tftp_address',
+        help='Tftp server ip-address.')
+    parser.add_argument('--image_filename', '-f', dest='image_filename',
+        help='Image filename.', required=True)
+    parser.add_argument('--action', '-o', dest='action',
+        help='Action Upgrade/Downgrade switch image.',
+        required=True, choices = ['copy', 'upgrade', 'downgrade', 'remove'])
+    args = parser.parse_args()
+    return Args(args)
+
+if __name__ == '__main__':
+
+    params = initialize_args()
+    nxapi_handler = initialize_nxapi_handler(params)
+
+    if params.action == 'copy':
+        if not copy_image_file(params, nxapi_handler):
+            print 'Failed to copy image file'
+            exit(-1)
+    elif params.action == 'upgrade':
+        # pre-requisite check before upgrading
+        if not verify_active_sessions(nxapi_handler):
+            print 'Failed to verify active sessions'
+            exit(-1)
+    elif params.action == 'downgrade':
+        if not check_image_incompatability(params, nxapi_handler):
+            print 'Failed to check image incompatability'
+            exit(-1)
+
+    if params.action in {'upgrade','downgrade'}:
+        if not check_install_all_impact(params, nxapi_handler):
+            print 'Failed to check install all impact.'
+            exit(-1)
+        if not copy_run_cfg_start_cfg(nxapi_handler):
+            print 'Failed to copy running-config to startup-config.'
+            exit(-1)
+        if not install_all_nxos_image(params, nxapi_handler):
+            print 'Failed to update switch image file.'
+            exit(-1)
+    exit(0)
